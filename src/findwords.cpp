6abb9383c3845d0c803ea9754b124be8e6aa34fe
John Kuras <w7og@yahoo.com>
2025-01-22 11:36:17 -0800

    added the ability to ignore permutations that are too short the argumant -m (--min-word-size) was added to specify the minimum length

--- src/findwords.cpp
+++ src/findwords.cpp
@@ -1,23 +1,27 @@
 //============================================================================
 // Name        : findwords.cpp
 // Author      : dad
-// Version     : Rev 0.0
+// Version     : Rev 0.1
 // Copyright   : (c)John Kuras 2025
 // License     : dwtfywwi
 // Description : find all word purmutations of a string
 //============================================================================
 // usage:
-// ./findwords ABCD |sort|uniq>in.txt
-// comm -23 in.txt <(aspell list < in.txt)
+// ./findwords FFERO |sort|uniq>in.txt ; comm -23 in.txt <(aspell list < in.txt) 2>/dev/null
+//
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+int minWordSize = 2;
 
 void permute(char *str, int n, int start, int len) {
     if (len == n) {
     	char temp2 = str[n];
     	str[n] = '\000';
-//        printf("%.*s\n", n, str + start); // Print the current permutation
-        printf("%.*s\n", n, str); // Print the current permutation
+        if ( n > minWordSize ) printf("%.*s\n", n, str); // Print the current permutation
         str[n] = temp2;
         if (len > 1 && n > 1){
         	permute(str+1, n-1, 0 , 0 );
@@ -43,13 +47,58 @@ int main( int argc, char **argv ) {
     char str[] = "ADOB"; // default string (used for debug)
     char * str2;
     int n; // Take permutations of size n
+    int c;
 
-	if (argc > 1) {
-			if ((strlen(argv[1]) > 0)) str2 = argv[1];
-		} else {
-			str2=str;
-		}
+    while (1) {
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"min-word-size",  required_argument, 0, 'm'},
+			{"help",  no_argument, 0, '?'},
+            {0,         0,                 0,  0 }
+        };
+
+        c = getopt_long(argc, argv, "m:?",
+                        long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch (c) {
+        case 0:
+            fprintf(stderr," option %s", long_options[option_index].name);
+            if (optarg)
+                fprintf(stderr," with arg %s", optarg);
+            fprintf(stderr,"\n");
+            break;
+
+        case 'm':
+            minWordSize = atoi((const char *)optarg);
+            if ( minWordSize < 0 ) {
+            	fprintf(stderr," warning : the minimum word size cannot be negative, using \"0\"\n");
+				minWordSize = 0;
+            }
+            break;
+
+        case '?':
+            fprintf(stderr, "Usage: %s [-m,--min-word-size n] [string]\n", argv[0]);
+            fprintf(stderr, "       finds all of the permutations of a string that are more that n characters in length.\n");
+            fprintf(stderr, "       default for n = 2\n");
+            fprintf(stderr, "       default for string = \"ADOB\"\n");
+            exit(EXIT_FAILURE);
+            break;
+
+        default:
+            fprintf(stderr," ?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+
+    if (optind < argc) {
+    	str2 = argv[optind++];
+    } else {
+		str2=str;
+	}
+
 	n = strlen(str2);
+	fprintf(stderr," finding all of the permutations of %s that are more than %i characters long\n",str2,minWordSize);
     permute(str2, n, 0, 0);
 
     return 0;
