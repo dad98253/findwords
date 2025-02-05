740474c47418c46f06b204fd638145abc37d4b35
John Kuras <w7og@yahoo.com>
2025-02-05 08:44:43 -0800

    add translations for Spanish and Malay

--- src/permute.cpp
+++ src/permute.cpp
@@ -14,11 +14,15 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <libintl.h>
 
 #include "../config.h"
 #define IN_MAIN
 #include "permute.h"
 #include "gitversion.h"
+
+#define _(String) gettext(String)
 
 void FindPermutations(char *str, int n, int start, int len);
 
@@ -28,6 +32,59 @@ int main( int argc, char **argv ) {
     char * str2;
     int n; // Take permutations of size n
     int c;
+    char * CurrentLocale = NULL;
+    char * CurrentDirnam = NULL;
+    char * CurrentDomain = NULL;
+
+    const char* env_var = getenv("LANGUAGE");
+     if (env_var != NULL) {
+             if ( strlen(env_var) ) {
+#ifdef DEBUG
+                     printf("Value of LANGUAGE: %s\n", env_var);
+#endif
+                     setenv("LANGUAGE", "", 1);
+             } else {
+#ifdef DEBUG
+                     printf("LANGUAGE is blank\n");
+#endif
+             }
+     }
+#ifdef DEBUG
+     else {
+             printf("LANGUAGE not found\n");
+     }
+#endif
+
+//      setlocale (LC_ALL, "en_US.UTF-8");
+     CurrentLocale = setlocale (LC_ALL, "ms_MY.UTF-8");
+#ifdef DEBUG
+     if (CurrentLocale) {
+             printf("locale set to %s\n",CurrentLocale);
+     } else {
+             fprintf(stderr,"setlocale failed\n");
+             exit(1);
+     }
+//    bindtextdomain (PACKAGE, LOCALEDIR);
+//     printf("LOCALEDIR = %s\n",LOCALEDIR);
+#endif
+     CurrentDirnam = bindtextdomain ("permute", "/home/dad/workspace/permute/locales");
+#ifdef DEBUG
+     if (CurrentDirnam) {
+             printf("dirname set to %s\n",CurrentDirnam);
+     } else {
+             fprintf(stderr,"bindtextdomain failed\n");
+             exit(2);
+     }
+#endif
+     CurrentDomain = textdomain ("permute");
+#ifdef DEBUG
+     if (CurrentDomain) {
+             printf("domain set to %s\n",CurrentDomain);
+     } else {
+             fprintf(stderr,"textdomain failed\n");
+             exit(3);
+     }
+#endif
 
     while (1) {
         int option_index = 0;
@@ -45,7 +102,7 @@ int main( int argc, char **argv ) {
 
         switch (c) {
         case 0:
-            fprintf(stderr," option %s", long_options[option_index].name);
+            fprintf(stderr,_(" option %s"), long_options[option_index].name);
             if (optarg)
                 fprintf(stderr," with arg %s", optarg);
             fprintf(stderr,"\n");
@@ -54,22 +111,22 @@ int main( int argc, char **argv ) {
         case 'm':
             minWordSize = atoi((const char *)optarg);
             if ( minWordSize < 0 ) {
-            	fprintf(stderr," warning : the minimum word size cannot be negative, using \"0\"\n");
+            	fprintf(stderr,_(" warning : the minimum word size cannot be negative, using \"0\"\n"));
 				minWordSize = 0;
             }
             break;
 
         case '?':
-            fprintf(stderr, "%s version %s, gittag %s\n\n", argv[0],VERSION,gittag);
-            fprintf(stderr, "Usage: %s [-m,--min-word-size n] [string]\n", argv[0]);
-            fprintf(stderr, "       finds all of the permutations of a string that are more that n characters in length.\n");
-            fprintf(stderr, "       default for n = 2\n");
-            fprintf(stderr, "       default for string = \"ADOB\"\n");
+            fprintf(stderr, _("%s version %s, gittag %s\n\n"), argv[0],VERSION,gittag);
+            fprintf(stderr, _("Usage: %s [-m,--min-word-size n] [string]\n"), argv[0]);
+            fprintf(stderr, _("       finds all of the permutations of a string that are more that n characters in length.\n"));
+            fprintf(stderr, _("       default for n = 2\n"));
+            fprintf(stderr, _("       default for string = \"ADOB\"\n"));
             exit(EXIT_FAILURE);
             break;
 
         default:
-            fprintf(stderr," ?? getopt returned character code 0%o ??\n", c);
+            fprintf(stderr,_(" ?? getopt returned character code 0%o ??\n"), c);
         }
     }
 
@@ -80,7 +137,7 @@ int main( int argc, char **argv ) {
 	}
 
 	n = strlen(str2);
-	fprintf(stderr," finding all of the permutations of %s that are more than %i characters long\n",str2,minWordSize);
+	fprintf(stderr,_(" finding all of the permutations of %s that are more than %i characters long\n"),str2,minWordSize);
     FindPermutations(str2, n, 0, 0);
 
     return 0;
